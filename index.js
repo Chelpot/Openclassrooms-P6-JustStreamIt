662cb7cee4074f9ad195cf17ebcd2cdd2b437ad8
Baquer Lucas <lucas.baquer@gmail.com>
2023-09-26 14:43:36 +0200

    update popup content to fit detail api results and fix css for content popup

--- index.js
+++ index.js
@@ -28,81 +28,81 @@ var pageAction = 1
 var pageBest = 1
 
 /* fantasy listener */
-leftBtnCat1.onclick = function() {
-  if(pageFantasy > 1){
+leftBtnCat1.onclick = function () {
+  if (pageFantasy > 1) {
     pageFantasy -= 1;
     logMovies(pageFantasy, CATEGORY1);
   }
 }
 
-rightBtnCat1.onclick = function() {
-  if(pageFantasy < dictPagesByCategory["maxPageFantasy"]){
+rightBtnCat1.onclick = function () {
+  if (pageFantasy < dictPagesByCategory["maxPageFantasy"]) {
     pageFantasy += 1
     logMovies(pageFantasy, CATEGORY1);
   }
 }
 
 /* history listener */
-leftBtnCat2.onclick = function() {
-  if(pageHistory > 1){
+leftBtnCat2.onclick = function () {
+  if (pageHistory > 1) {
     pageHistory -= 1;
     logMovies(pageHistory, CATEGORY2);
   }
 }
 
-rightBtnCat2.onclick = function() {
-  if(pageHistory < dictPagesByCategory["maxPageHistory"]){
+rightBtnCat2.onclick = function () {
+  if (pageHistory < dictPagesByCategory["maxPageHistory"]) {
     pageHistory += 1
     logMovies(pageHistory, CATEGORY2);
   }
 }
 
 /* action listener */
-leftBtnCat3.onclick = function() {
-  if(pageAction > 1){
+leftBtnCat3.onclick = function () {
+  if (pageAction > 1) {
     pageAction -= 1;
     logMovies(pageAction, CATEGORY3);
   }
 }
 
-rightBtnCat3.onclick = function() {
-  if(pageAction < dictPagesByCategory["maxPageHistory"]){
+rightBtnCat3.onclick = function () {
+  if (pageAction < dictPagesByCategory["maxPageHistory"]) {
     pageAction += 1
     logMovies(pageAction, CATEGORY3);
   }
 }
 
 /* best movies listener */
-leftBtnBest.onclick = function() {
-  if(pageBest > 1){
+leftBtnBest.onclick = function () {
+  if (pageBest > 1) {
     pageBest -= 1;
     logMoviesBest(pageBest);
   }
 }
 
-rightBtnBest.onclick = function() {
-  if(pageBest < maxPageBest){
+rightBtnBest.onclick = function () {
+  if (pageBest < maxPageBest) {
     pageBest += 1
     logMoviesBest(pageBest);
   }
 }
 
 /* close popup listeners */
-span.onclick = function() {
-    popup.style.display = "none";
-  }
+span.onclick = function () {
+  popup.style.display = "none";
+}
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == popup) {
     popup.style.display = "none";
   }
 }
 
-document.addEventListener("click", function(e) {
+document.addEventListener("click", function (e) {
   if (e.target.classList.contains("film")) {
     title = e.target.textContent;
     openPopup(title)
-    
+
   }
 });
 
@@ -111,38 +111,45 @@ async function openPopup(title) {
   const response = await fetch("http://localhost:8000/api/v1/titles/?" + new URLSearchParams({
     title: title
   }));
-  data = await response.json()
-  data = data.results[0]
+
+  data = await response.json();
+  url_detail = data.results[0].url
+
+  const response_detail = await fetch(url_detail);
+  data = await response_detail.json();
   popup.style.display = "inline-block";
 
 
   // Load data in popup
+  document.getElementById("popup-img").src = data.image_url;
   document.getElementById("popup-title").textContent = data.title;
-  document.getElementById("popup-year").textContent = "Année : " + data.year;
-  document.getElementById("popup-votes").textContent = "Nombre de votes : " + data.votes;
+  document.getElementById("popup-genres").textContent = "Genres : " + Array.from(data.genres).join(' | ');
+  document.getElementById("popup-publishedDate").textContent = "Film publié le : " + data.date_published;
+  document.getElementById("popup-rated").textContent = "Rated : " + data.rated;
   document.getElementById("popup-imbScore").textContent = "Score sur le site Imb : " + data.imdb_score;
   document.getElementById("popup-directors").textContent = "Directeurs : " + Array.from(data.directors).join(' | ');
   document.getElementById("popup-actors").textContent = "Acteurs : " + Array.from(data.actors).join(' | ');
-  document.getElementById("popup-writers").textContent = "Scénaristes : " + Array.from(data.writers).join(' | ');
-  document.getElementById("popup-genres").textContent = "Genres : " + Array.from(data.genres).join(' | ');
-  document.getElementById("popup-img").src = data.image_url;
+  document.getElementById("popup-duration").textContent = "Durée du film : " + data.duration + " Minutes";
+  document.getElementById("popup-countries").textContent = "Pays d'origine : " + Array.from(data.countries).join(' | ');
+  document.getElementById("popup-boxoffice").textContent = "Résultats au Box Office : " + data.worldwide_gross_income + "$";
+  document.getElementById("popup-description").textContent = "Résumé : " + data.long_description;
 }
 
 async function logMoviesBest(nbPage) {
   const response = await fetch("http://localhost:8000/api/v1/titles/?" + new URLSearchParams({
     page: nbPage,
     sort_by: "-imdb_score"
-}));
+  }));
 
-const movies = await response.json();
-maxPageBest = Math.trunc(movies.count/5);
+  const movies = await response.json();
+  maxPageBest = Math.trunc(movies.count / 5);
 
-var films = document.getElementsByClassName("film-best");
-films = Array.from(films)
-films.forEach((element, index) => {
-  element.textContent = movies.results[index].title;
-  element.src = movies.results[index].image_url;
-});
+  var films = document.getElementsByClassName("film-best");
+  films = Array.from(films)
+  films.forEach((element, index) => {
+    element.textContent = movies.results[index].title;
+    element.src = movies.results[index].image_url;
+  });
 
 }
 
@@ -156,7 +163,7 @@ async function logMovies(nbPage, categoryName) {
   }))
 
   const data = await response.json();
-  dictPagesByCategory[`maxPage${categoryName}`] = Math.trunc(data.count/5);
+  dictPagesByCategory[`maxPage${categoryName}`] = Math.trunc(data.count / 5);
   categoryName = categoryName.toLowerCase()
   /* For each img in our Carousel we assign the corresponding datas */
   var moviesElements = document.getElementsByClassName(`film-${categoryName}`);
